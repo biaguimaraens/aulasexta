5fc6b2cf5dbb38f175744ef6eee3eee0a6a70635
bianca <biaguimaraens@gmail.com>
2019-02-01 11:25:23 -0200

    aula da carla

--- src/app/pages/identidadessecretas/identidadessecretas.component.ts
+++ src/app/pages/identidadessecretas/identidadessecretas.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdentidadessecretasComponent implements OnInit {
 
+passwordError: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(login){
+  	console.log(login.value.usuario);
+  	console.log(login.value.senha);
+  	console.log(login.value.mae);
+	console.log(login);
+  }
+
+  checkPassword(senha){
+    if(senha.value.length!=6)
+      this.passwordError = true;
+    else
+      this.passwordError = false;
   }
 
 }
